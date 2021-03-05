cad6fd6ae33286b72b71d5799232e253a46ce7a0
S Reddy <sachitgreddy@email.com>
2021-03-04 19:29:08 -0500

    added firebase config

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -1,7 +1,14 @@
 const firebase = require("firebase/app")
 require("firebase/firestore")
 
-const firebaseConfig = {} // replace
+const firebaseConfig = {
+  apiKey: "AIzaSyAvv2YDvaVPYpinLaaCc32AH8jFrwohwig",
+  authDomain: "kiei-451-6a40f.firebaseapp.com",
+  projectId: "kiei-451-6a40f",
+  storageBucket: "kiei-451-6a40f.appspot.com",
+  messagingSenderId: "678177254416",
+  appId: "1:678177254416:web:b29c5d9c7915c07604ee95"
+}; // replace
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
